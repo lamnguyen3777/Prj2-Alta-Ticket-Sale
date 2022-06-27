344d5bc3a0203e857319906785295b79807ae6d0
lamnguyen3777 <73375708+lamnguyen3777@users.noreply.github.com>
2022-06-27 08:03:32 +0700

    Update route.tsx

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,18 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from "../view/index";
-import Check from "../view/checkTicket";
-import TicketList from "../view/ticketList";
-import Setting from "../view/setting";
-
-const Router = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ticketList" element={<TicketList />} />
-      <Route path="/check" element={<Check />} />
-      <Route path="/setting" element={<Setting />} />
-    </Routes>
-  );
-};
-
-export default Router;
+export interface IRoute {
+  path: string;
+  title: string;
+  icon: string;
+}
